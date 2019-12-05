8e859af50427316f5b3a2bcce3cf9ca0ff74a683
Harun Ozceyhan <harunozceyhan@gmail.com>
2019-12-05 18:59:59 +0300

    chore(siparis): unncessary import delete

--- src/main/java/com/smart/sbo/domain/beden/Siparis.java
+++ src/main/java/com/smart/sbo/domain/beden/Siparis.java
@@ -2,11 +2,9 @@ package com.smart.sbo.domain.beden;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
