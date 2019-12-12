7bb138bc72603ef5be3375ec0a30518d39481244
Harun Ozceyhan <harunozceyhan@gmail.com>
2019-12-12 19:18:24 +0300

    fix(test): annotation delete

--- src/test/java/com/smart/sbo/controller/SiparisControllerTest.java
+++ src/test/java/com/smart/sbo/controller/SiparisControllerTest.java
@@ -1,12 +1,11 @@
 package com.smart.sbo.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
@@ -20,7 +19,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
-@SpringBootTest
 @WebMvcTest
 public class SiparisControllerTest {
 
