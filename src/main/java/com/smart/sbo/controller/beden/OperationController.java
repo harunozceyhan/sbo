f1690b25631bfd3283a1e78d7a0976dce5033a8c
Harun Özceyhan <harunozceyhan@smartiys.com>
2020-01-14 02:37:04 +0300

    fix: hateoas pagedmodel response embeded key fix for empty response

--- src/main/java/com/smart/sbo/controller/beden/OperationController.java
+++ src/main/java/com/smart/sbo/controller/beden/OperationController.java
@@ -1,11 +1,11 @@
 package com.smart.sbo.controller.beden;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 
@@ -28,11 +28,12 @@ public class OperationController {
     PagedResourcesAssembler<Operation> pagedResourcesAssembler;
 
     @RequestMapping(path = "/operation", method = RequestMethod.GET, produces = "application/hal+json")
-    public ResponseEntity<PagedModel<EntityModel<Operation>>> get(@Param("adi") String adi, @Param("kodu") String kodu,
+    public ResponseEntity<PagedModel<?>> get(@Param("adi") String adi, @Param("kodu") String kodu,
             @Param("startOperationDate") @DateTimeFormat(pattern = "dd-MM-yyyy") Date startOperationDate,
             @Param("endOperationDate") @DateTimeFormat(pattern = "dd-MM-yyyy") Date endOperationDate, Pageable p) {
-        PagedModel<EntityModel<Operation>> collModel = pagedResourcesAssembler
-                .toModel(operationService.getOperationList(adi, kodu, startOperationDate, endOperationDate, p));
-        return ResponseEntity.ok(collModel);
+        Page<Operation> operation = operationService.getOperationList(adi, kodu, startOperationDate, endOperationDate,
+                p);
+        return ResponseEntity.ok(operation.isEmpty() ? pagedResourcesAssembler.toEmptyModel(operation, Operation.class)
+                : pagedResourcesAssembler.toModel(operation));
     }
 }
